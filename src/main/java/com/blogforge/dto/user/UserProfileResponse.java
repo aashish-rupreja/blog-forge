f72447c5624c730b8d665c9f2094fdecae6e5c36
aashish-rupreja <aashish.rupreja4797@gmail.com>
2026-07-08 12:09:30 +0530

    Rectify user profile response

--- src/main/java/com/blogforge/dto/user/UserProfileResponse.java
+++ src/main/java/com/blogforge/dto/user/UserProfileResponse.java
@@ -1,13 +1,15 @@
 package com.blogforge.dto.user;
 
+import com.blogforge.dto.BaseResponse;
+
 import java.time.LocalDate;
 
 public record UserProfileResponse(
+        BaseResponse baseResponse,
         String firstName,
         String lastName,
         String username,
         String profilePicLink,
-        String bio,
-        LocalDate joinedOn
+        String bio
 ) {
 }
