46d6cfc70e13a2602e22cc46168582b7cc27063a
aashish-rupreja <aashish.rupreja4797@gmail.com>
2026-07-09 04:07:44 +0530

    Allow search for multiple category/tags by names

--- src/main/java/com/blogforge/repository/CategoryRepository.java
+++ src/main/java/com/blogforge/repository/CategoryRepository.java
@@ -3,7 +3,9 @@ package com.blogforge.repository;
 import com.blogforge.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.UUID;
 
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+    Collection<Category> findByNameIn(Collection<String> categoryNames);
 }
--- src/main/java/com/blogforge/repository/TagRepository.java
+++ src/main/java/com/blogforge/repository/TagRepository.java
@@ -3,7 +3,9 @@ package com.blogforge.repository;
 import com.blogforge.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.UUID;
 
 public interface TagRepository extends JpaRepository<Tag, UUID> {
+    Collection<Tag> findByNameIn(Collection<String> tagNames);
 }
