c819eb876221f8f1b44947ab1bc3b65a7be76687
aashish-rupreja <aashish.rupreja4797@gmail.com>
2026-07-11 00:47:49 +0530

    Add custom exception when entity to create already exists

---
+++ src/main/java/com/blogforge/exception/EntityAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.blogforge.exception;
+
+public class EntityAlreadyExistsException extends RuntimeException {
+    public EntityAlreadyExistsException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/blogforge/exception/GlobalExceptionHandler.java
+++ src/main/java/com/blogforge/exception/GlobalExceptionHandler.java
@@ -3,17 +3,50 @@ package com.blogforge.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest req) {
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        Map<String, String> validationErrors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach((error) -> {
+            validationErrors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        ExceptionResponse er = new ExceptionResponse(
+                Instant.now(),
+                httpStatus.value(),
+                validationErrors.toString(),
+                req.getServletPath()
+        );
+        return new ResponseEntity<>(er, httpStatus);
+    }
+
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ExceptionResponse> handleEntityNotFoundException(EntityNotFoundException e, HttpServletRequest req) {
         HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+        ExceptionResponse er = new ExceptionResponse(
+                Instant.now(),
+                httpStatus.value(),
+                e.getMessage(),
+                req.getServletPath()
+        );
+        return new ResponseEntity<>(er, httpStatus);
+    }
+
+    @ExceptionHandler(EntityAlreadyExistsException.class)
+    public ResponseEntity<ExceptionResponse> handleEntityAlreadyExistsException(EntityAlreadyExistsException e, HttpServletRequest req) {
+        HttpStatus httpStatus = HttpStatus.CONFLICT;
         ExceptionResponse er = new ExceptionResponse(
                 Instant.now(),
                 httpStatus.value(),
