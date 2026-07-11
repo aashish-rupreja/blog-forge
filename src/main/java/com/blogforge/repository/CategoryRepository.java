a23486061ebc748017099832b709fc434cb75953
aashish-rupreja <aashish.rupreja4797@gmail.com>
2026-07-11 17:33:44 +0530

    Add custom method to fetch Category by name

--- src/main/java/com/blogforge/repository/CategoryRepository.java
+++ src/main/java/com/blogforge/repository/CategoryRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Collection;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CategoryRepository extends JpaRepository<Category, UUID>, JpaSpecificationExecutor<Category> {
     Collection<Category> findByNameIn(Collection<String> categoryNames);
+
+    Optional<Category> findByNameIgnoreCase(String categoryName);
 }
