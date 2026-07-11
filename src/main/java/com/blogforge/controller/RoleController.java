5a4ceafa904883da3bfa94f144f69831cb2c3152
aashish-rupreja <aashish.rupreja4797@gmail.com>
2026-07-11 18:18:00 +0530

    Add endpoints for operations on Role

--- src/main/java/com/blogforge/controller/RoleController.java
+++ src/main/java/com/blogforge/controller/RoleController.java
@@ -1,15 +1,16 @@
 package com.blogforge.controller;
 
+import com.blogforge.dto.GenericResponse;
+import com.blogforge.dto.role.CreateRoleRequest;
 import com.blogforge.dto.role.RoleResponse;
 import com.blogforge.pagination.PagedResponse;
 import com.blogforge.pagination.PaginationRequestParams;
 import com.blogforge.service.RoleService;
 import com.blogforge.specification.role.RoleSpecificationParams;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RoleController {
@@ -27,5 +28,23 @@ public class RoleController {
             ) {
         PagedResponse<RoleResponse> roles = roleService.getAll(reqParams, specParams);
         return new ResponseEntity<>(roles, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/api/v1/roles/{name}")
+    public ResponseEntity<RoleResponse> getByName(@PathVariable String name) {
+        RoleResponse rr = roleService.getByName(name);
+        return new ResponseEntity<>(rr, HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/api/v1/roles")
+    public ResponseEntity<RoleResponse> create(@Valid @RequestBody CreateRoleRequest dto) {
+        RoleResponse rr = roleService.create(dto);
+        return new ResponseEntity<>(rr, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(path = "/api/v1/roles")
+    public ResponseEntity<GenericResponse> delete(@Valid @RequestBody DeleteRoleRequest dto) {
+        GenericResponse gr = roleService.delete(dto);
+        return new ResponseEntity<>(gr, HttpStatus.OK);
     }
 }
