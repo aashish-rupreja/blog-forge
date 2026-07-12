36dfe5c007ec61141f9e48168c299979a0d692cc
aashish-rupreja <aashish.rupreja4797@gmail.com>
2026-07-12 20:39:37 +0530

    Fields can be null in partial update

--- src/main/java/com/blogforge/dto/blog/UpdateBlogRequest.java
+++ src/main/java/com/blogforge/dto/blog/UpdateBlogRequest.java
@@ -6,21 +6,16 @@ import jakarta.validation.constraints.*;
 import java.util.Set;
 
 public record UpdateBlogRequest(
-        @NotBlank(message = "{blog.title.notBlank}")
         @Size(min = 5, max = 50, message = "{blog.title.size}")
         String title,
 
-        @NotBlank(message = "{blog.content.notBlank}")
         @Size(min = 100, max = 1000, message = "{blog.content.size}")
         String content,
 
-        @NotNull(message = "{blog.enableComments.notNull}")
-        boolean enableComments,
+        Boolean enableComments,
 
-        @NotBlank(message = "{blog.blogStatus.notBlank}")
         BlogStatus blogStatus,
 
-        @NotEmpty(message = "{blog.categoryIds.notEmpty}")
         @Size(min = 1, max = 5, message = "{blog.categoryIds.size}")
         Set<String> categoryIds,
 
