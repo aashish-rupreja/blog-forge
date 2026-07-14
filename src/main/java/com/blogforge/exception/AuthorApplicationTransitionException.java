32af623ef285efbf43572d5eb2e001ab9a4d4ccc
aashish-rupreja <aashish.rupreja4797@gmail.com>
2026-07-15 04:18:18 +0530

    Throw exception for illegal transition

---
+++ src/main/java/com/blogforge/exception/AuthorApplicationTransitionException.java
@@ -0,0 +1,7 @@
+package com.blogforge.exception;
+
+public class AuthorApplicationTransitionException extends RuntimeException {
+    public AuthorApplicationTransitionException(String message) {
+        super(message);
+    }
+}
