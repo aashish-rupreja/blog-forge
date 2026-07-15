91a972848d4c3563c95f3caa7acfc097c1aa25fc
aashish-rupreja <aashish.rupreja4797@gmail.com>
2026-07-16 03:57:44 +0530

    Remove @Component because unnecessary

--- src/main/java/com/blogforge/security/CustomUserDetails.java
+++ src/main/java/com/blogforge/security/CustomUserDetails.java
@@ -10,10 +10,9 @@ import org.springframework.stereotype.Component;
 import java.util.Collection;
 import java.util.List;
 
-@Component
 public class CustomUserDetails implements UserDetails {
 
-    private User user;
+    private final User user;
 
     public CustomUserDetails(User user) {
         this.user = user;
