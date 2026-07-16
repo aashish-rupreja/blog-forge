80b02582e5dff6c0fd37ef843e573b3d5036d753
aashish-rupreja <aashish.rupreja4797@gmail.com>
2026-07-16 06:15:40 +0530

    Rectify: Remove @Component since already injected in SecurityConfig and remove invalid call to doFilter

--- src/main/java/com/blogforge/security/JWTFilter.java
+++ src/main/java/com/blogforge/security/JWTFilter.java
@@ -6,12 +6,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
 public class JWTFilter extends OncePerRequestFilter {
 
     private static final Logger LOG = LoggerFactory.getLogger(JWTFilter.class);
@@ -21,7 +19,7 @@ public class JWTFilter extends OncePerRequestFilter {
 
         if(request.getHeader("Authorization") == null || !request.getHeader("Authorization").startsWith("Bearer ")) {
             LOG.trace("Authorization header or Bearer token not present");
-            doFilter(request, response, filterChain);
+            filterChain.doFilter(request, response);
             return;
         }
 
