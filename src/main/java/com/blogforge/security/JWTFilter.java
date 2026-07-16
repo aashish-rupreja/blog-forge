23056563736518ffc598d4596f7728373df84b39
aashish-rupreja <aashish.rupreja4797@gmail.com>
2026-07-16 06:06:32 +0530

    Add trace logs for debugging

--- src/main/java/com/blogforge/security/JWTFilter.java
+++ src/main/java/com/blogforge/security/JWTFilter.java
@@ -4,6 +4,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,14 +13,20 @@ import java.io.IOException;
 
 @Component
 public class JWTFilter extends OncePerRequestFilter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JWTFilter.class);
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        if(!request.getHeader("Authorization").startsWith("Bearer")) {
+        if(request.getHeader("Authorization") == null || !request.getHeader("Authorization").startsWith("Bearer ")) {
+            LOG.trace("Authorization header or Bearer token not present");
             doFilter(request, response, filterChain);
+            return;
         }
 
         String token = request.getHeader("Authorization").substring(7);
+        LOG.trace("Bearer token found");
 
         // if token invalid do filter
 
